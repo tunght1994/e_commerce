bb5b3ab961fd99d2bf812c1a6eccea65d8384389
tunght1994 <thanhtung@21g.in>
2022-02-06 15:51:32 +0700

    'thanhtung'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
 import './sass/index.scss'
 import reportWebVitals from './reportWebVitals';
 
